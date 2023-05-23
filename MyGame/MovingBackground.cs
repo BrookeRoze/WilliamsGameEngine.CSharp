1aad09cc2584e1d8a77b24b089be51c4174a09f0
Brooke Roze <rozebro@rsd407.org>
2023-05-22 19:24:10 -0700

    5/22/23

--- MyGame/MovingBackground.cs
+++ MyGame/MovingBackground.cs
@@ -10,30 +10,30 @@ using SFML.Window;
 
 namespace MyGame
 {
-    internal class MovingBackground
-    {
-        private const float Speed = 0.3f;
+    //internal class MovingBackground
+    //{
+    //    private const float Speed = 0.3f;
         
-        private readonly Sprite background = new();
-        public MovingBackground(Vector2f pos)
-        {
-            background.Texture = Game.GetTexture("background.png");
-            background.Position = new Vector2f(300, 400);
-        }
-        public override void Draw()
-        {
-            Game.RenderWindow.Draw(background);
-        }
-        public override void Update(Time elapsed)
-        {
-            int Elapsed = elapsed.AsMilliseconds();
-            Vector2f pos = background.Position;
-            float x = pos.X;
-            float y = pos.Y;
+    //    private readonly Sprite background = new();
+    //    public MovingBackground(Vector2f pos)
+    //    {
+    //        background.Texture = Game.GetTexture("background.png");
+    //        background.Position = new Vector2f(300, 400);
+    //    }
+    //    public override void Draw()
+    //    {
+    //        Game.RenderWindow.Draw(background);
+    //    }
+    //    public override void Update(Time elapsed)
+    //    {
+    //        int Elapsed = elapsed.AsMilliseconds();
+    //        Vector2f pos = background.Position;
+    //        float x = pos.X;
+    //        float y = pos.Y;
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Left)) { x -= Speed * Elapsed; }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.Right)) { x += Speed * Elapsed; }
-            background.Position = new Vector2f(pos.X - Speed * Elapsed, pos.Y);
-        }
-    }
+    //        if (Keyboard.IsKeyPressed(Keyboard.Key.Left)) { x -= Speed * Elapsed; }
+    //        if (Keyboard.IsKeyPressed(Keyboard.Key.Right)) { x += Speed * Elapsed; }
+    //        background.Position = new Vector2f(pos.X - Speed * Elapsed, pos.Y);
+    //    }
+    //}
 }
